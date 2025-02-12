9524b6c8f345210787b145364dca9f3bc9e9d08b
nihad <nihad.guliyev@gmail.com>
2025-02-12 23:44:59 +0100

    Qa

---
+++ src/Java_ev.java
@@ -0,0 +1,93 @@
+public class Java_ev {
+   public static void main(String[] args) {
+//        byte a=1;
+//        short b=2;
+//        int c=10;
+//        long d=500;
+//        float f=20.5f ;
+//        double g= 5.5;
+//        char h='a';
+//        boolean isEqual= true;
+
+//       int[] a={10,20,30,40,50,100};
+//       System.out.println(
+//            a[0]+a[a.length-1]
+//       );
+
+//       String[] a={"Nihad","Guliyev"};
+//       System.out.println(
+//               a[0]+" "+a[1]
+
+
+//       );
+//       int a=5;
+//       int b=6;
+//       a+=b;
+//       b=a-b;
+//       a-=b;
+//
+////       int c;
+////       c=a;
+////       a=b;
+////       b=c;
+//       System.out.println( a);
+//       System.out.println(b);
+
+//       int yash=30;
+//       String ad="John Doe";
+//       System.out.println("salam menim adim " +ad+ "dir ve menim " +yash+ " yasim var");
+
+//       int[] a = {4, 6, 9, 12, 5, 13};
+//       for (int i = 0; i < a.length; i++) {
+//           if (a[i]%2==0) {
+//               System.out.println(a[i]+" cut eddeddir");
+//           }
+//       else {
+//               System.out.println(a[i]+" tek ededdir");
+//           }
+//       }
+//        int a=10;
+//        int b=3;
+//        int c=a*b;
+//        if(a*b>100) System.out.println("ela");
+//        else if (a*b>70) {
+//            System.out.println("orta");
+//        }
+//        else {
+//            System.out.println("belli olmayan netice");
+//
+//        }
+//       System.out.println(c>100? "ela":c>70? "orta":c>50?"zeif": "belli olmayan netice ");
+
+//        Tapsiriq 7
+
+//        public static class MinMaxFinder {
+//         public static void main(String[] args) {
+//            int[] numbers = {34, 12, 56, 78, 3, 89, 21, 5};
+//
+//            int min = numbers[0];
+//            int max = numbers[0];
+//
+//            for (int num : numbers) {
+//               if (num < min) {
+//                  min = num;
+//               }
+//               if (num > max) {
+//                  max = num;
+//               }
+//            }
+
+//            System.out.println("Array: " + Arrays.toString(numbers));
+//            System.out.println("Ən kiçik rəqəm: " + min);
+//            System.out.println("Ən böyük rəqəm: " + max);
+
+
+
+               }
+            }
+
+
+
+
+
+
---
+++ src/avtomobil.java
@@ -0,0 +1,56 @@
+public class avtomobil {
+    public static void main(String[] args) {
+
+        class carAvto {
+            String ad;
+            String reng;
+            int qapi;
+            String model;
+            int il;
+            int say;
+
+
+        }
+        carAvto auto1=new carAvto();
+        auto1.ad="Nazlifan";
+        auto1.model="Nax";
+        auto1.il=2020;
+
+        auto1.reng="ag";
+
+
+        System.out.println(auto1.ad);
+        System.out.println(auto1.model);
+//
+//        carAvto auto2=new carAvto();
+//        auto2.ad="hyundai";
+//        auto2.model="Sonata";
+//        auto2.il=2008;
+//        auto2.qapi=5;
+//        auto2.reng="goy";
+//        auto2.say=5;
+//        System.out.println(auto2.ad);
+//        System.out.println(auto2.model);
+//        System.out.println(auto2.il);
+//        System.out.println(auto2.qapi);
+//        System.out.println(auto2.reng);
+//        System.out.println(auto2.say);
+
+//        carAvto auto3=new carAvto();
+//        auto3.ad="Toyota";
+//        auto3.model="Carolla";
+//        auto3.il=2009;
+//        auto3.qapi=5;
+//        auto3.reng="qara";
+//        auto3.say=5;
+//        System.out.println(auto3.ad);
+//        System.out.println(auto3.model);
+//        System.out.println(auto3.il);
+//        System.out.println(auto3.qapi);
+//        System.out.println(auto3.reng);
+//        System.out.println(auto3.say);
+
+
+    }
+
+}
